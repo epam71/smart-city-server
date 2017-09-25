10f70f2ecbf035c6ce383a6ac6f981c788b9954d
Roksolana <roksolana-mykhalenych@mail.ru>
2017-09-25 15:19:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/auth-test', authModule.checkUser);
 app.post('/projects/:id/likes', dbAgent.postLikes);
 app.post('/news/:id/likes', dbAgent.postLikes);
 app.post('/projects/:id/comments', dbAgent.postComments);
-app.delete('/projects/:id/comments', dbAgent.deleteComments);
+app.delete('/projects/:id/comments/commentId', dbAgent.deleteComments);
 app.post('/news/:id/comments', dbAgent.postComments);
 app.post('/notifications', mailSender.sendEmail);
 
