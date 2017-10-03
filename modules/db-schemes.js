1b501fcf586878266112b8e7804beea8c8b98974
Roksolana <roksolana-mykhalenych@mail.ru>
2017-10-03 23:00:07 +0300

    Update db-schemes.js

--- modules/db-schemes.js
+++ modules/db-schemes.js
@@ -20,10 +20,10 @@ const projects = new Schema({
     },
     desc: String,
     image: String,
-    imageKey: [{
+    imageKey: {
         type: String,
         required: true
-    }],
+    },
     goals: String,
     result: String,
     rating: { 
@@ -80,10 +80,10 @@ const news = new Schema({
         required: true
     },
     image: String,
-    imageKey: [{
+    imageKey: {
         type: String,
         required: true
-    }],
+    },
     desc: String,
     approved: Boolean,
     date: {
